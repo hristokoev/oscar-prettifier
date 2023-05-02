2418ad6fff0e3e94c0671fe06e4525a47fa56ae8
Hristo Koev <iamhristokoev@gmail.com>
2023-05-02 21:58:56 +0200

    add desc to classes

--- includes/readClass.js
+++ includes/readClass.js
@@ -3,7 +3,25 @@ const readClass = (flightClass, partner) => {
 	const clone = flightClass.cloneNode(true);
 	const parent = flightClass.parentNode;
 	const container = document.createElement('div');
-	clone.textContent = partner ? "Partner class" : CLASSESAFKL[classText];
+	clone.textContent = partner ? `Partner class ${classText}` : `${CLASSESAFKL[classText]} ${classText}`;
+	if (clone.textContent != `Partner class ${classText}`) {
+		switch (CLASSESAFKL[classText]) {
+			case "Economy":
+				flightClass.textContent = `ECO ${classText}`;
+				break;
+			case "Business":
+				flightClass.textContent = `BIZ ${classText}`;
+				break;
+			case "La Première":
+				flightClass.textContent = `E/1 ${classText}`;
+				break;
+			case "Premium Comfort":
+				flightClass.textContent = `E/P ${classText}`;
+				break;
+		}
+	} else {
+		flightClass.textContent = `UNK ${classText}`;
+	}
 	clone.className = 'popup';
 	parent.replaceChild(container, flightClass);
 	container.className = 'container';
