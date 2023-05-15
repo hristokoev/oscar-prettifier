1e8bcf0382ea5089250545ca5de6cc4d189050e4
hristokoev <52719283+hristokoev@users.noreply.github.com>
2023-05-15 11:58:08 +0200

    update for deny (help HE commands)

--- includes/etc/containsMultiple.js
+++ includes/etc/containsMultiple.js
@@ -1,8 +1,15 @@
-const containsMultiple = (isSafe, string, commands, safeCommands) => {
+const containsMultiple = (isSafe, string, commands, safeCommands, denyCommands) => {
 	let commandsLength = commands.length;
 	let safeCommandsLength = safeCommands.length;
+	let denyCommandsLength = denyCommands.length;
 	if (string.length == 0) {
 		return true;
+	}
+	for (let i = 0; i < denyCommandsLength; i++) {
+		if (string.includes(denyCommands[i])) {
+			isSafe = false;
+			return false;
+		}
 	}
 	for (let i = 0; i < commandsLength; i++) {
 		if (string.includes(commands[i])) {
